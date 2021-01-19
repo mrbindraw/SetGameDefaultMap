f1d4bffa3567d9f1191a1cf0c887cf4339fee5fd
Andrew Bindraw <mrbindraw@gmail.com>
2021-01-19 11:42:20 +0300

    added UE_LOG for class UNativeGameInstance

--- Source/SetGameDefaultMap/NativeGameInstance.cpp
+++ Source/SetGameDefaultMap/NativeGameInstance.cpp
@@ -7,6 +7,10 @@ void UNativeGameInstance::Init()
 {
 	Super::Init();
 
+#if PLATFORM_WINDOWS
+	UE_LOG(LogTemp, Log, TEXT(__FUNCTION__));
+#endif
+
 	UGameMapsSettings::SetGameDefaultMap(TEXT("/Game/Maps/NewMap2.NewMap2"));
 }
 
@@ -14,10 +18,16 @@ void UNativeGameInstance::OnStart()
 {
 	Super::OnStart();
 
+#if PLATFORM_WINDOWS
+	UE_LOG(LogTemp, Log, TEXT(__FUNCTION__));
+#endif
 }
 
 void UNativeGameInstance::Shutdown()
 {
 	Super::Shutdown();
 
+#if PLATFORM_WINDOWS
+	UE_LOG(LogTemp, Log, TEXT(__FUNCTION__));
+#endif
 }
